5de72788f893682411cc981e8376f5a37187f193
SERGIO <idealis.tfm@gmail.com>
2025-08-19 04:06:35 +0000

    chore(husky): clean pre-commit hook (v9) + remove legacy .eslintignore

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', 'build', 'coverage'],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+];
